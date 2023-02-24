a5e8741119deabe5d1a0749f2affd083fc4458c5
marcogracie <marco.gracie22@gmail.com>
2023-02-23 21:18:19 -0500

    Initial Commit

---
+++ PocketAdvisor/PocketAdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketAdvisorApp.swift
+//  PocketAdvisor
+//
+//  Created by Marco Gracie on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct PocketAdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
