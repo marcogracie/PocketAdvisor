161d6965971274ee3d288de764021f695ccba0c2
marcogracie <marco.gracie22@gmail.com>
2023-04-12 18:46:40 -0400

    So all the login and everything is done, next step is showing our users a chart of their portfolio on the main screen when they log in.

--- PocketAdvisor/Views/MainScreenView.swift
+++ PocketAdvisor/Views/MainScreenView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct MainScreenView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            BackgroundView()
+            //ChartView()
+            //ForegroundView(stock: <#T##Stock#>, config: <#T##Binding<ChartConfig>#>)
+        }
     }
 }
 
